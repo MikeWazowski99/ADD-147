7ad187fae331057a11f724363bbed2da7223e0b1
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2025-02-21 23:16:38 -0600

    Add files via upload

---
+++ learn02-start/scripts/script.js
@@ -0,0 +1,9 @@
+function myFunction2() {
+    document.getElementById("sample").innerHTML = "Changed from the function in the external script section"
+}
+
+// This is my single line comment
+
+/*
+This is my multi-line comment
+*/
